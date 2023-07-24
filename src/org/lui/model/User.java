ad931b8e9c3148487132d6aa49bbd23f3fdd49ec
Kyle <kylebenedict.lui@finastra.com>
2023-07-24 10:08:22 +0800

    Added getter and setter for the name field

--- src/org/lui/model/User.java
+++ src/org/lui/model/User.java
@@ -3,5 +3,15 @@ package org.lui.model;
 public class User {
 		
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 	
 }
